88bb348db1b6c8c20d0f88f4becbd4e26f8aed8a
rsinghaal <rohit.singhaal@gmail.com>
2022-09-29 18:12:58 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import App1 from './App1';
+import App4 from './App4';
+import App5 from './App5';
+import App6 from './App6';
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App4 />
+  </StrictMode>
+);
