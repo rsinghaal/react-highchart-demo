3a62b2d4042501f340af7738daa67185f271fadb
rsinghaal <rohit.singhaal@gmail.com>
2022-09-30 12:50:06 +0530

    2 files modified

--- src/App7.js
+++ src/App7.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from 'highcharts';
-import pdata from 'data.json';
-import pdata1 from 'data1.json';
+import pdata from './data.json';
+import pdata1 from './data1.json';
 
 class App7 extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       // To avoid unnecessary update keep all options in the state.
       chartOptions: {
